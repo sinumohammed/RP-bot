a5f908278abd0f02e37f004047122ded7f8d3c28
sinumohammed <sinumohammed@gmail.com>
2018-12-08 18:17:47 +0530

    initial code merge

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../rpbotbi.zip');
+var kuduApi = 'https://rpbotbi.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$rpbotbi';
+var password = '4lLGekFk54hX99PAH2D6vdvEZ7ygprdeYqAc7hey53trm46HFi814ZJmEpm5';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('rpbotbi publish');
+  } else {
+    console.error('failed to publish rpbotbi', err);
+  }
+});
